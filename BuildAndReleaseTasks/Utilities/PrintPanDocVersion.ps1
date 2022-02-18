55c193e5e6f18d5bb3b79d0bb941a13e2870d67f
James Stephens <james@codevanguard.com>
2022-02-17 19:50:36 -0500

    Adding licensing and support information

--- BuildAndReleaseTasks/Utilities/PrintPanDocVersion.ps1
+++
@@ -1,4 +0,0 @@
-# This file is part of PanDoc for Azure Pipelines,
-# Copyright (C) 2020-2022 Code Vanguard LLC
-
-pandoc -v
--- ci/CopyVstsLibToTasks.ps1
+++ ci/CopyVstsLibToTasks.ps1
@@ -1,7 +1,18 @@
 # This file is part of PanDoc for Azure Pipelines,
 # Copyright (C) 2020-2022 Code Vanguard LLC
-
 #
+# PanDoc for Azure Pipelines is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# PanDoc for Azure Pipelines is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PanDoc for Azure Pipelines.  If not, see <http://www.gnu.org/licenses/>.
+
 # CopyVstsLibToTasks.ps1
 # 
 # Copies the VstsTaskSdk to each task
